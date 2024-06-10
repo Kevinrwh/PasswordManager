7b8d9e2a688e1ce965bc59f439617b5a5bb839ca
Kevin Ramos <Kevramos93@yahoo.com>
2024-06-09 22:28:44 -0400

    add cryptography to encrypt passwords

--- password_manager.py
+++ password_manager.py
@@ -4,11 +4,26 @@
 
 from cryptography.fernet import Fernet
 
-# TBD
+# Uncomment and run write_key() with load_key() commented out to create key file
+'''# Define a key for us
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
+
+write_key()
+'''
+
+# Once running the program once to write the key, comment write_key out
+# and run the rest of the program
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
 
 # View all stored passwords
 def view():
@@ -17,14 +32,15 @@ def view():
         
         for line in f.readlines():
 
-            # retrieve the line
+            # retrieve the line. rstrip removes carriage return
             data = line.rstrip()
 
-            # split the username and password with |
+            # split the username and password with |, assumes username and password do not have a |
             user, pwd = data.split("|")
 
             # Display this username and password
-            print("User: ", user, "| Password: ", pwd)
+            print("User: ", user, "| Password: ", 
+                  fer.decrypt(pwd.encode()).decode())
 
 
 # Add the new username & password combo to the passwords text file
@@ -37,8 +53,8 @@ def add():
     # Open passwords file in 'append' mode
     with open("passwords.txt", "a") as f:
 
-        # Write the username and password to the file
-        f.write(name + "|" + pwd + "\n")
+        # Write the username and encrypted password to the file
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 # Delete all passwords
 def delete():
@@ -46,8 +62,6 @@ def delete():
     # Deletes the contents of the file if it exists or creates an empty file and deletes it
     open("passwords.txt", "w").close()
 
-# Master password providing access to viewing all passwords in the passwords file
-master_pwd = input("What is your master password? ")
 
 # Prompt the user for view, add, or quit
 mode = input("Would you like to view your existing passwords or add a new one? You can also press q to quit. ")
@@ -63,13 +77,7 @@ while True:
     # View existing passwords
     if mode == "view":
 
-        # Prompt the user for the master password
-        master_user = input("What is the master password? ")
-
-        if master_user == master_pwd:
-            view()
-        else:
-            print("Invalid password. ")
+        view()
     
     # Add a username & password combo
     elif mode == "add":
